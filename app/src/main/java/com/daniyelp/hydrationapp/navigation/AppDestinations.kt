2320bdd3b1293f2e93baf349fefbddd2cce4cdbc
unknown <shisonokyojin@icloud.com>
2022-01-26 22:51:58 +0200

    Add destinations

---
+++ app/src/main/java/com/daniyelp/hydrationapp/navigation/AppDestinations.kt
@@ -0,0 +1,19 @@
+package com.daniyelp.hydrationapp.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.filled.LocalDrink
+import androidx.compose.ui.graphics.vector.ImageVector
+
+object AppDestinations {
+    sealed class Screen(val route: String)
+    object Home: Screen("home") {
+        sealed class HomeScreen(route: String, val icon: ImageVector, val title: String): Screen(route)
+        object History: HomeScreen("history", Icons.Default.History, "History")
+        object Today: HomeScreen("today", Icons.Default.LocalDrink, "Today")
+    }
+    object Settings: Screen("settings")
+    object Units: Screen("units")
+    object DailyGoal: Screen("dailygoal")
+    object Container: Screen("container")
+}
